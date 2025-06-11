146ca41a0f7fa0fccb23cf0893abcb51c6d4428a
Yuhang <yuhang@topsoftint.com>
2025-06-11 16:39:47 +0800

    evaluation curves

---
+++ experiments/robot/libero/draw_energy.py
@@ -0,0 +1,28 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+with open('ours.pkl', 'rb') as f:
+    ours = pickle.load(f)
+
+
+with open('baseline.pkl', 'rb') as f:
+    baseline = pickle.load(f)
+
+
+E_base = baseline
+E_hnn = ours
+
+dE_base = E_base - E_base[0]
+dE_hnn  = E_hnn  - E_hnn[0]
+
+ts = np.arange(len(dE_base))
+plt.figure()
+plt.plot(ts, dE_base, label='Baseline ΔE')
+plt.plot(ts, dE_hnn,  label='With HNN ΔE')
+plt.xlabel('Timestep')
+plt.ylabel('ΔDynamic Energy')
+plt.legend()
+plt.savefig('dynamic_energy_drift.pdf')
+plt.close()
