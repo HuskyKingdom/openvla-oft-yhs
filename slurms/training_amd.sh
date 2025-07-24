04c3eb7cbbd50dc6e34af0e70d66d9961567cb70
Yuhang <yuhang@topsoftint.com>
2025-07-24 16:05:10 +0800

    amd training test

--- slurms/training_amd.sh
+++ slurms/training_amd.sh
@@ -11,7 +11,7 @@ cd /work1/aiginternal/yuhang/openvla-oft-yhs
 export WANDB_API_KEY=0bdbd99b1136358467ed2d03e9a6ba5a5b2a11a8
 export HF_HOME=/work1/aiginternal/yuhang/
 
-FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE" torchrun --standalone --nnodes 1 --nproc-per-node 8 vla-scripts/finetune_withHNN.py \
+FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE" torchrun --standalone --nnodes 1 --nproc-per-node 4 vla-scripts/finetune_withHNN.py \
   --vla_path openvla/openvla-7b \
   --data_root_dir /work1/aiginternal/yuhang/modified_libero_rlds \
   --dataset_name libero_4_task_suites_no_noops \
