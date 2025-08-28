9ff9f376e4f9b621ed7150cbfd67b63412bae133
HuskyKingdom <1595025647@qq.com>
2025-08-28 13:17:15 +0800

    energy finetuning

--- experiments/robot/libero/run_energy_vis.py
+++ experiments/robot/libero/run_energy_vis.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # 读取 tfrecord
-tfrecord_path = "/work1/aiginternal/yuhang/modified_libero_rlds//1.0.0/liber_o10-train.tfrecord-00000-of-00032"
+tfrecord_path = "/work1/aiginternal/yuhang/modified_libero_rlds/1.0.0/liber_o10-train.tfrecord-00000-of-00032"
 raw_dataset = tf.data.TFRecordDataset(tfrecord_path)
 
 for raw_record in raw_dataset.take(1):  # 先看一个样本
