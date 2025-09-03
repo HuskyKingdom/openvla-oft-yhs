f9a41f430bddb6b4599ae9ee64abffaaa16c6811
HuskyKingdom <1595025647@qq.com>
2025-09-03 14:47:53 +0800

    energy vis testing

--- vla-scripts/retriving_data.py
+++ vla-scripts/retriving_data.py
@@ -414,6 +414,8 @@ def run_forward_pass(
                 layer_actions.append(current_actions)
             action_head.eval() 
 
+        print(vla.llm_dim)
+        assert 1==2
         print(ground_truth_actions)
         torch.save(context_hidden, "energy_vis/context_hidden_ts1.pt")
         torch.save(batch["pixel_values"], "energy_vis/pixel_values.pt")
