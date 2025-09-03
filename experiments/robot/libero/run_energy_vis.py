cc1a351cfa7005122d826c6881d1e2e9353fa860
HuskyKingdom <1595025647@qq.com>
2025-09-03 13:07:35 +0800

    energy vis testing

--- experiments/robot/libero/run_energy_vis.py
+++ experiments/robot/libero/run_energy_vis.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
+import torch
 
-# 读取 tfrecord
-tfrecord_path = "/work1/aiginternal/yuhang/modified_libero_rlds/libero_10_no_noops/1.0.0/liber_o10-train.tfrecord-00000-of-00032"
-raw_dataset = tf.data.TFRecordDataset(tfrecord_path)
-
-for raw_record in raw_dataset.take(1):  # 先看一个样本
-    example = tf.train.Example()
-    example.ParseFromString(raw_record.numpy())
-    print(example)
+# 载入
+pixel_values = torch.load("energy_vis/pixel_values.pt")
+print(pixel_values.shape)
--- vla-scripts/retriving_data.py
+++ vla-scripts/retriving_data.py
@@ -414,7 +414,7 @@ def run_forward_pass(
                 layer_actions.append(current_actions)
             action_head.eval() 
 
-        print(ground_truth_actions,batch["labels"])
+        print(ground_truth_actions)
         torch.save(context_hidden, "energy_vis/context_hidden_ts1.pt")
         torch.save(batch["pixel_values"], "energy_vis/pixel_values.pt")
         processor = AutoProcessor.from_pretrained("/work1/aiginternal/yuhang/openvla-oft-yhs/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b3+lr-0.0005+lora-r32+dropout-0.0--image_aug--energy_finetuned--200000_chkpt", trust_remote_code=True)
