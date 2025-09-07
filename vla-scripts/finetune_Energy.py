28d503c13b148416594153f9a0f333455c59676c
Yuhang <yuhang@topsoftint.com>
2025-09-07 13:48:12 +0800

    energy

--- vla-scripts/finetune_Energy.py
+++ vla-scripts/finetune_Energy.py
@@ -487,7 +487,7 @@ def run_forward_pass(
 
         # E_pos = energy_model(context_hidden,ground_truth_actions,energy_mask)
         swap_loss, E_pos_mean, E_neg_mean = energy_inbatch_swap_infonce(energy_model,context_hidden,ground_truth_actions, energy_mask)
-        reg = F.mse_loss(E_pos_mean, torch.zero_like(E_pos_mean))
+        reg = F.mse_loss(E_pos_mean, torch.zeros_like(E_pos_mean))
 
         # L_neg, E_neg = compute_negative_energy(energy_model,ground_truth_actions,layer_actions,0.2,context_hidden,E_pos,energy_mask)
         
