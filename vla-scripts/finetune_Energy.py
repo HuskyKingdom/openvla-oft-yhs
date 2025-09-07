e490de188aabedd9409e215792d64d5baa9f4144
Yuhang <yuhang@topsoftint.com>
2025-09-07 14:42:59 +0800

    energy

--- vla-scripts/finetune_Energy.py
+++ vla-scripts/finetune_Energy.py
@@ -485,15 +485,17 @@ def run_forward_pass(
         # )
         energy_mask = None
 
-        # E_pos = energy_model(context_hidden,ground_truth_actions,energy_mask)
-        swap_loss, E_pos_mean, E_neg_mean = energy_inbatch_swap_infonce(energy_model,context_hidden,ground_truth_actions, energy_mask)
-        reg = F.mse_loss(E_pos_mean, torch.zeros_like(E_pos_mean))
+        E_pos = energy_model(context_hidden,ground_truth_actions,energy_mask)
+        # swap_loss, E_pos_mean, E_neg_mean = energy_inbatch_swap_infonce(energy_model,context_hidden,ground_truth_actions, energy_mask)
+        reg = F.mse_loss(E_pos, torch.zeros_like(E_pos))
 
-        # L_neg, E_neg = compute_negative_energy(energy_model,ground_truth_actions,layer_actions,0.2,context_hidden,E_pos,energy_mask)
+        L_neg, E_neg_mean = compute_negative_energy(energy_model,ground_truth_actions,layer_actions,0.2,context_hidden,E_pos,energy_mask)
         
-        energy_loss = swap_loss
+        energy_loss = L_neg + 0.02 * reg
 
-        print(swap_loss)
+        print(L_neg ,0.02 * reg)
+
+        E_pos_mean = E_pos.mean()
        
         
         # #  positive loss and negative loss
