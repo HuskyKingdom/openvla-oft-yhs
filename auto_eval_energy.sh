db547f2367fcea90e93fe4dea103db8d73ec5ca0
Yuhang <yuhang@topsoftint.com>
2025-09-23 10:47:35 +0800

    energy freeze eval

--- auto_eval_energy.sh
+++ auto_eval_energy.sh
@@ -8,21 +8,21 @@ FARM_USER=sgyson10
 echo "Evaluating spatial ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_spatial --e_decoding True --task_label w_energy_spatial
+    --task_suite_name libero_spatial --e_decoding True --task_label w_energy_spatial_0.3
 
 echo "Evaluating object ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_object --e_decoding True --task_label w_energy_object
+    --task_suite_name libero_object --e_decoding True --task_label w_energy_object_0.3
 
 echo "Evaluating goal ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_goal --e_decoding True --task_label w_energy_goal
+    --task_suite_name libero_goal --e_decoding True --task_label w_energy_goal_0.3
 
 echo "Evaluating long ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_10 --e_decoding True --task_label w_energy_10
+    --task_suite_name libero_10 --e_decoding True --task_label w_energy_10_0.3
 
 
--- experiments/robot/openvla_utils.py
+++ experiments/robot/openvla_utils.py
@@ -785,7 +785,7 @@ def action_contrastive_fusion(selected_layer_action,final_layer_action,coffes):
 
 
 
-def one_step_energy_correction_seq(energy_head, h, A_bc, energy_mask, alpha=0.1, clip_frac=0.2,
+def one_step_energy_correction_seq(energy_head, h, A_bc, energy_mask, alpha=0.3, clip_frac=0.2,
                                    act_range=None, correct_first_only=False):
     """
     A_bc: [H, Da] (numpy array or torch tensor)
