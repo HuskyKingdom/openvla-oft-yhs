0d96c823e9cf892febfb1f4ddfe0e61730488c69
HuskyKingdom <1595025647@qq.com>
2025-10-14 10:41:52 +0800

    energy:k correction experiments

--- auto_eval_energy_k.sh
+++ auto_eval_energy_k.sh
@@ -28,23 +28,23 @@ echo N | python experiments/robot/libero/run_libero_eval.py \
 
 
 
-echo "Evaluating spatial ------------------------------"
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_spatial --e_decoding True --energy_k 1 --task_label energy_k1_spatial
+# echo "Evaluating spatial ------------------------------"
+# echo N | python experiments/robot/libero/run_libero_eval.py \
+#     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+#     --task_suite_name libero_spatial --e_decoding True --energy_k 1 --task_label energy_k1_spatial
 
 
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_spatial --e_decoding True --energy_k 2 --task_label energy_k2_spatial
+# echo N | python experiments/robot/libero/run_libero_eval.py \
+#     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+#     --task_suite_name libero_spatial --e_decoding True --energy_k 2 --task_label energy_k2_spatial
 
 
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_spatial --e_decoding True --energy_k 3 --task_label energy_k3_spatial
+# echo N | python experiments/robot/libero/run_libero_eval.py \
+#     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+#     --task_suite_name libero_spatial --e_decoding True --energy_k 3 --task_label energy_k3_spatial
 
 
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_spatial --e_decoding True --energy_k 4 --task_label energy_k4_spatial
+# echo N | python experiments/robot/libero/run_libero_eval.py \
+#     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+#     --task_suite_name libero_spatial --e_decoding True --energy_k 4 --task_label energy_k4_spatial
 
