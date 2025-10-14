713f31a6a1bef92e7f168ceca1f83d9c8c302b1f
HuskyKingdom <1595025647@qq.com>
2025-10-14 10:47:34 +0800

    energy:k correction experiments

--- auto_eval_energy_k.sh
+++ auto_eval_energy_k.sh
@@ -8,22 +8,22 @@ FARM_USER=sgyson10
 echo "Evaluating long ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_long --e_decoding True --energy_k 1 --task_label energy_k1_long
+    --task_suite_name libero_10 --e_decoding True --energy_k 1 --task_label energy_k1_long
 
 
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_long --e_decoding True --energy_k 2 --task_label energy_k2_long
+    --task_suite_name libero_10 --e_decoding True --energy_k 2 --task_label energy_k2_long
 
 
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_long --e_decoding True --energy_k 3 --task_label energy_k3_long
+    --task_suite_name libero_10 --e_decoding True --energy_k 3 --task_label energy_k3_long
 
 
 echo N | python experiments/robot/libero/run_libero_eval.py \
     --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_long --e_decoding True --energy_k 4 --task_label energy_k4_long
+    --task_suite_name libero_10 --e_decoding True --energy_k 4 --task_label energy_k4_long
 
 
 
