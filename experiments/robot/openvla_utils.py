7470d3dbdb29fc8ee5e48f3b5b5177b872799113
HuskyKingdom <1595025647@qq.com>
2025-10-27 13:45:07 +0800

    trajectory vis testing

--- experiments/robot/openvla_utils.py
+++ experiments/robot/openvla_utils.py
@@ -960,8 +960,9 @@ def k_step_energy_correction_seq(
         A = (A - step).detach()
 
 
+    E_corrected = energy_head(h, A, energy_mask)
     # print action energy
-    print(f"Action Energy: {E.item():.10f}")
+    print(f"Action Energy: {E.item():.10f} | Corrected Action Energy: {E_corrected.item():.10f}")
     
     return A.squeeze(0).detach().cpu().to(torch.float32).numpy()
 
