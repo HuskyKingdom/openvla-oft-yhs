dbd72ca571f98588b26b4c8293a1ca40d489fc5e
HuskyKingdom <1595025647@qq.com>
2025-11-06 14:06:35 +0800

    ablation

--- auto_eval_energy.sh
+++ auto_eval_energy.sh
@@ -7,22 +7,22 @@ FARM_USER=sgyson10
 
 echo "Evaluating spatial ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained \
     --task_suite_name libero_spatial --e_decoding True --task_label w_energy_spatial_0.3
 
 echo "Evaluating object ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained \
     --task_suite_name libero_object --e_decoding True --task_label w_energy_object_0.3
 
 echo "Evaluating goal ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained \
     --task_suite_name libero_goal --e_decoding True --task_label w_energy_goal_0.3
 
 echo "Evaluating long ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained \
     --task_suite_name libero_10 --e_decoding True --task_label w_energy_10_0.3
 
 
--- auto_eval_energy_k.sh
+++ auto_eval_energy_k.sh
@@ -5,47 +5,49 @@ echo "Running Evaluations Automatically ------------------------------"
 FARM_USER=sgyson10
 
 
+echo "Evaluating spatial ------------------------------"
+echo N | python experiments/robot/libero/run_libero_eval.py \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_spatial --e_decoding True  --energy_alpha 1.0 --task_label energy_alpha_1.0_spatial
+
+
+echo N | python experiments/robot/libero/run_libero_eval.py \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_spatial --e_decoding True  --energy_alpha 0.5 --task_label energy_alpha_0.5_spatial
+
+
+echo "Evaluating long ------------------------------"
+echo N | python experiments/robot/libero/run_libero_eval.py \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_10 --e_decoding True  --energy_alpha 1.0 --task_label energy_alpha_1.0_10
+
+
+echo N | python experiments/robot/libero/run_libero_eval.py \
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_10 --e_decoding True  --energy_alpha 0.5 --task_label energy_alpha_0.5_10
+
+
 
 echo "Evaluating goal ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_goal --e_decoding True --energy_k 1 --task_label energy_k1_goal
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_goal --e_decoding True  --energy_alpha 1.0 --task_label energy_alpha_1.0_goal
 
 
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_goal --e_decoding True --energy_k 2 --task_label energy_k2_goal
-
-
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_goal --e_decoding True --energy_k 3 --task_label energy_k3_goal
-
-
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_goal --e_decoding True --energy_k 4 --task_label energy_k4_goal
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_goal --e_decoding True  --energy_alpha 0.5 --task_label energy_alpha_0.5_goal
 
 
 
 echo "Evaluating object ------------------------------"
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_object --e_decoding True --energy_k 1 --task_label energy_k1_object
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_object --e_decoding True --energy_alpha 1.0 --task_label energy_alpha_1.0_object
 
 
 echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_object --e_decoding True --energy_k 2 --task_label energy_k2_object
-
-
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_object --e_decoding True --energy_k 3 --task_label energy_k3_object
-
-
-echo N | python experiments/robot/libero/run_libero_eval.py \
-    --pretrained_checkpoint /mnt/nfs/$FARM_USER/openvla-oft-yhs/ckpoints/pre-trained_universal \
-    --task_suite_name libero_object --e_decoding True --energy_k 4 --task_label energy_k4_object
+    --pretrained_checkpoint /home/aup/YuhangWorkspace/openvla-oft-yhs/ckpts/pre-trained  \
+    --task_suite_name libero_object --e_decoding True --energy_alpha 0.5 --task_label energy_alpha_0.5_object
 
 
