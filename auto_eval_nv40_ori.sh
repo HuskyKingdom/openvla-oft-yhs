680aed1e3ed8edc09842448fd3fda228acd08b98
Yuhang <yuhang@topsoftint.com>
2026-01-13 13:31:36 +0800

    openvla exp

--- auto_eval_nv40_ori.sh
+++ auto_eval_nv40_ori.sh
@@ -18,12 +18,12 @@ sed -i 's/use_swap: false/use_swap: true/' experiments/robot/libero/LIBERO-PRO/e
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
-  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_swap 
+  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_swap  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-10   \
-  --task_suite_name libero_10   --center_crop True --task_label ori_10_swap 
+  --task_suite_name libero_10   --center_crop True --task_label ori_10_swap  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 # object
 sed -i 's/use_swap: true/use_swap: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
@@ -32,12 +32,12 @@ sed -i 's/use_object: false/use_object: true/' experiments/robot/libero/LIBERO-P
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
-  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_object 
+  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_object  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-10   \
-  --task_suite_name libero_10   --center_crop True --task_label ori_10_object 
+  --task_suite_name libero_10   --center_crop True --task_label ori_10_object  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 # lan
@@ -46,12 +46,12 @@ sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBE
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
-  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_lan 
+  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_lan  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-10   \
-  --task_suite_name libero_10   --center_crop True --task_label ori_10_lan 
+  --task_suite_name libero_10   --center_crop True --task_label ori_10_lan  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 # task
 sed -i 's/use_language: true/use_language: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
@@ -59,12 +59,12 @@ sed -i 's/use_task: false/use_task: true/' experiments/robot/libero/LIBERO-PRO/e
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
-  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_task 
+  --task_suite_name libero_spatial   --center_crop True --task_label ori_spatial_task  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-10   \
-  --task_suite_name libero_10   --center_crop True --task_label ori_10_task 
+  --task_suite_name libero_10   --center_crop True --task_label ori_10_task  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 sed -i 's/use_task: true/use_task: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
