46fb1d5d2c79d658ad6de1084cff54459902b831
Yuhang <yuhang@topsoftint.com>
2026-01-13 13:33:17 +0800

    openvla exp

--- auto_eval_nv40_ori.sh
+++ auto_eval_nv40_ori.sh
@@ -12,8 +12,8 @@ echo "Evaluating OpenVLA ------------------------------"
 
 
 # swap
-sed -i 's/use_environment: true/use_environment: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
-sed -i 's/use_swap: false/use_swap: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_environment: true/use_environment: false/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_swap: false/use_swap: true/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
@@ -26,8 +26,8 @@ python experiments/robot/libero/run_libero_eval.py   \
   --task_suite_name libero_10   --center_crop True --task_label ori_10_swap  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 # object
-sed -i 's/use_swap: true/use_swap: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
-sed -i 's/use_object: false/use_object: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_swap: true/use_swap: false/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_object: false/use_object: true/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
 python experiments/robot/libero/run_libero_eval.py   \
@@ -41,8 +41,8 @@ python experiments/robot/libero/run_libero_eval.py   \
 
 
 # lan
-sed -i 's/use_object: true/use_object: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
-sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_object: true/use_object: false/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_language: false/use_language: true/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
@@ -54,8 +54,8 @@ python experiments/robot/libero/run_libero_eval.py   \
   --task_suite_name libero_10   --center_crop True --task_label ori_10_lan  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 # task
-sed -i 's/use_language: true/use_language: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
-sed -i 's/use_task: false/use_task: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_language: true/use_language: false/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_task: false/use_task: true/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 python experiments/robot/libero/run_libero_eval.py   \
   --model_family openvla   --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial   \
@@ -67,4 +67,4 @@ python experiments/robot/libero/run_libero_eval.py   \
   --task_suite_name libero_10   --center_crop True --task_label ori_10_task  --evaluation_config_path /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
-sed -i 's/use_task: true/use_task: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_task: true/use_task: false/' /home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhsexperiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
