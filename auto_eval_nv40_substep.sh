61f4669a895916cc48ca334b83a99e7e82447f41
Yuhang <yuhang@topsoftint.com>
2026-02-10 14:00:07 +0800

    apd nan trace

--- auto_eval_nv40_substep.sh
+++ auto_eval_nv40_substep.sh
@@ -38,12 +38,12 @@ sed -i 's/use_swap: false/use_swap: true/' experiments/robot/libero/LIBERO-PRO/e
 
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py  \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_object --e_decoding False --save_video False  --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label substep_object_swap --use_eos_detection True
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label substep_goal_swap --use_eos_detection True
 
@@ -54,12 +54,12 @@ sed -i 's/use_object: false/use_object: true/' experiments/robot/libero/LIBERO-P
 
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_object --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label substep_object_object --use_eos_detection True
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label substep_goal_object --use_eos_detection True
 
@@ -69,12 +69,12 @@ sed -i 's/use_object: true/use_object: false/' experiments/robot/libero/LIBERO-P
 sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_object --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label substep_object_lan --use_eos_detection True
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07\
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07\
   --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label substep_goal_lan --use_eos_detection True
 
@@ -84,12 +84,12 @@ sed -i 's/use_language: true/use_language: false/' experiments/robot/libero/LIBE
 sed -i 's/use_task: false/use_task: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07\
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07\
   --task_suite_name libero_object --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label substep_object_task --use_eos_detection True
 
 python experiments/robot/libero/run_libero_pro_eval_substep.py \
-  --pretrained_checkpoint ckpt/ckpoints/openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla--150000_chkpt/ --substep_completion_threshold 0.07 \
+  --pretrained_checkpoint openvla-7b+libero_4_task_suites_no_noops+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--substep--substep_vla_s_35--200000_chkpt/ --substep_completion_threshold 0.07 \
   --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition True\
   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label substep_goal_task --use_eos_detection True
 
