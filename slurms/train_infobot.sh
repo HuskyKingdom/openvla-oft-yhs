2ba3659abf1259d2f29ff10aafb2c9b108ed0311
Yuhang <yuhang@topsoftint.com>
2026-02-19 10:37:18 +0800

    Fix SLURM partition to mi3008xl

--- slurms/train_infobot.sh
+++ slurms/train_infobot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=infobot_vla
-#SBATCH --time=48:00:00
-#SBATCH --partition=vlm
+#SBATCH --time=12:00:00
+#SBATCH --partition=mi3008xl
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=1
