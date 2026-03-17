26bdd1a5a4098a81d59b03a3a1dee4da87060e8d
Yuhang <yuhang@topsoftint.com>
2026-03-17 16:37:00 +0800

    eval simple vla

--- auto_eval_nv40_plus2.sh
+++ auto_eval_nv40_plus2.sh
@@ -6,6 +6,8 @@ FILE_PATH="experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml"
 PRETRAINED_CHECKPOINT="Haozhan72/openvla-oft-libero10-traj1-rl"
 TASK_LABEL_PREFIX="simplevla"
 USE_EOS_DETECTION=False
+EVAL_SCRIPT="experiments/robot/libero/run_libero_pro_eval_javas.py"
+USE_PROPRIO=False
 
 
 
@@ -18,15 +20,15 @@ sed -i 's/use_environment: true/use_environment: false/' experiments/robot/liber
 sed -i 's/use_swap: false/use_swap: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
-# python experiments/robot/libero/run_libero_pro_eval_javas.py  \
+# python $EVAL_SCRIPT  \
 #   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
 #   --task_suite_name libero_spatial --e_decoding False --save_video False  --use_substep_decomposition True\
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_swap --use_eos_detection $USE_EOS_DETECTION
+#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_swap --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
-python experiments/robot/libero/run_libero_pro_eval_javas.py \
+python $EVAL_SCRIPT \
   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
   --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition True\
-  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_swap --use_eos_detection $USE_EOS_DETECTION
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_swap --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
 
 # object
@@ -34,44 +36,44 @@ sed -i 's/use_swap: true/use_swap: false/' experiments/robot/libero/LIBERO-PRO/e
 sed -i 's/use_object: false/use_object: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
 
-# python experiments/robot/libero/run_libero_pro_eval_javas.py \
+# python $EVAL_SCRIPT \
 #   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
 #   --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition True\
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_object --use_eos_detection $USE_EOS_DETECTION
+#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_object --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
-python experiments/robot/libero/run_libero_pro_eval_javas.py \
+python $EVAL_SCRIPT \
   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
   --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition True\
-  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_object --use_eos_detection $USE_EOS_DETECTION
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_object --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
 
 # lan
 sed -i 's/use_object: true/use_object: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
-# python experiments/robot/libero/run_libero_pro_eval_javas.py \
+# python $EVAL_SCRIPT \
 #   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
 #   --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition True\
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_lan --use_eos_detection $USE_EOS_DETECTION
+#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_lan --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
-python experiments/robot/libero/run_libero_pro_eval_javas.py \
+python $EVAL_SCRIPT \
   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07\
   --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition True\
-  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_lan --use_eos_detection $USE_EOS_DETECTION
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_lan --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
 
 # task
 sed -i 's/use_language: true/use_language: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 sed -i 's/use_task: false/use_task: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
-# python experiments/robot/libero/run_libero_pro_eval_javas.py \
+# python $EVAL_SCRIPT \
 #   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07\
 #   --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition True\
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_task --use_eos_detection $USE_EOS_DETECTION
+#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_task --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
-python experiments/robot/libero/run_libero_pro_eval_javas.py \
+python $EVAL_SCRIPT \
   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold 0.07 \
   --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition True\
-  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_task --use_eos_detection $USE_EOS_DETECTION
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_task --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO
 
 sed -i 's/use_task: true/use_task: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
