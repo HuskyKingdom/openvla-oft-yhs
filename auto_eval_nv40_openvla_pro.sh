4d80ba0a62d4250f9f4a4a10f75917d0e1bea380
Yuhang <yuhang@topsoftint.com>
2026-04-01 15:28:33 +0800

    rl training

--- auto_eval_nv40_openvla_pro.sh
+++ auto_eval_nv40_openvla_pro.sh
@@ -6,6 +6,7 @@ CKPT_SPATIAL="openvla/openvla-7b-finetuned-libero-spatial"
 CKPT_OBJECT="openvla/openvla-7b-finetuned-libero-object"
 CKPT_GOAL="openvla/openvla-7b-finetuned-libero-goal"
 CKPT_10="openvla/openvla-7b-finetuned-libero-10"
+CKPT_apd="/home/yuhang/Warehouse/Yuhangworkspace/openvla-oft-yhs/ckpt/landmarked_ckpoints/apd_regressive_200k"
 TASK_LABEL_PREFIX="openvla"
 EVAL_SCRIPT="experiments/robot/libero/run_libero_pro_eval.py"
 USE_PROPRIO=False
@@ -23,28 +24,28 @@ bash experiments/robot/libero/LIBERO-PRO/reset_eval_config.sh $FILE_PATH
 
 
 # lan
-# sed -i 's/use_environment: true/use_environment: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
-# sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_environment: true/use_environment: false/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
+sed -i 's/use_language: false/use_language: true/' experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml
 
-# python $EVAL_SCRIPT \
-#   --pretrained_checkpoint $CKPT_OBJECT \
-#   --task_suite_name libero_object --save_video $SAVE_VIDEO \
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label ${TASK_LABEL_PREFIX}_object_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
+python $CKPT_apd \
+  --pretrained_checkpoint $CKPT_OBJECT \
+  --task_suite_name libero_object --save_video $SAVE_VIDEO \
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_object --task_label ${TASK_LABEL_PREFIX}_object_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
 
-# python $EVAL_SCRIPT \
-#   --pretrained_checkpoint $CKPT_GOAL \
-#   --task_suite_name libero_goal --save_video $SAVE_VIDEO \
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label ${TASK_LABEL_PREFIX}_goal_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_GOAL \
+  --task_suite_name libero_goal --save_video $SAVE_VIDEO \
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label ${TASK_LABEL_PREFIX}_goal_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
 
-# python $EVAL_SCRIPT \
-#   --pretrained_checkpoint $CKPT_SPATIAL \
-#   --task_suite_name libero_spatial --save_video $SAVE_VIDEO \
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_SPATIAL \
+  --task_suite_name libero_spatial --save_video $SAVE_VIDEO \
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_spatial --task_label ${TASK_LABEL_PREFIX}_spatial_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
 
-# python $EVAL_SCRIPT \
-#   --pretrained_checkpoint $CKPT_10 \
-#   --task_suite_name libero_10 --save_video $SAVE_VIDEO \
-#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_10 \
+  --task_suite_name libero_10 --save_video $SAVE_VIDEO \
+  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_10 --task_label ${TASK_LABEL_PREFIX}_10_lan --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT --use_bddl_language $USE_BDDL_LANGUAGE
 
 
 # object
