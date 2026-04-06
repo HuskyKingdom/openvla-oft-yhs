c02f9a3be542ab035fcddcccbb9f82d3f4c2bb43
Yuhang <yuhang@topsoftint.com>
2026-04-06 17:04:10 +0800

    discrete exp

--- SimpleVLA-RL/apd_trail.sh
+++ SimpleVLA-RL/apd_trail.sh
@@ -1,8 +1,8 @@
-export SFT_MODEL_PATH="/work1/chunyilee/yuhang/openvla-oft-yhs/landmarked_ckpoints/apd_discrete_200k"
+export SFT_MODEL_PATH="/work1/chunyilee/yuhang/openvla-oft-yhs/landmarked_ckpoints/apd_discrete_160k"
 export CKPT_PATH="./exp_out"
 export APD_PLANS_PATH="/work1/chunyilee/yuhang/openvla-oft-yhs/APD_plans_scaled.json"
 export CONTRASTIVE_REWARD_COEF=2
-export DATASET_NAME="libero_spatial"
+export DATASET_NAME="libero_4_task_suites"
 export EXPERIMENT_NAME="grpo-apd-liberospatial-run1"
 export PROJECT_NAME="openvla-oft-rl"
 export NUM_GPUS=8
