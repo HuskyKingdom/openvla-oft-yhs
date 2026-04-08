690e207de2a17ed74c40e744421040a0da35a645
Yuhang <yuhang@topsoftint.com>
2026-04-08 14:14:53 +0800

    oftplus language dropping exp

--- auto_eval_language_dropping.sh
+++ auto_eval_language_dropping.sh
@@ -131,51 +131,7 @@ bash experiments/robot/libero/LIBERO-PRO/reset_eval_config.sh $FILE_PATH
 # ===========================================================================
 echo "OFT Baseline: NULL LANGUAGE (Language Dropping) ------------------------------"
 
-# swap
-sed -i 's/use_swap: false/use_swap: true/' $FILE_PATH
-
-python $EVAL_SCRIPT \
-  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
-  --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
-  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_spatial \
-  --task_label oft_spatial_swap_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
-  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
-  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction False \
-  2>&1 | tee ckpts/oft_spatial_swap_nulllang.txt
-
-python $EVAL_SCRIPT \
-  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
-  --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
-  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_10 \
-  --task_label oft_10_swap_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
-  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
-  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
-  2>&1 | tee ckpts/oft_10_swap_nulllang.txt
-
-# object
-sed -i 's/use_swap: true/use_swap: false/' $FILE_PATH
-sed -i 's/use_object: false/use_object: true/' $FILE_PATH
-
-python $EVAL_SCRIPT \
-  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
-  --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
-  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_spatial \
-  --task_label oft_spatial_object_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
-  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
-  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
-  2>&1 | tee ckpts/oft_spatial_object_nulllang.txt
-
-python $EVAL_SCRIPT \
-  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
-  --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
-  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_10 \
-  --task_label oft_10_object_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
-  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
-  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
-  2>&1 | tee ckpts/oft_10_object_nulllang.txt
-
 # lan
-sed -i 's/use_object: true/use_object: false/' $FILE_PATH
 sed -i 's/use_language: false/use_language: true/' $FILE_PATH
 
 python $EVAL_SCRIPT \
@@ -218,6 +174,50 @@ python $EVAL_SCRIPT \
   --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
   2>&1 | tee ckpts/oft_10_task_nulllang.txt
 
+# object
 sed -i 's/use_task: true/use_task: false/' $FILE_PATH
+sed -i 's/use_object: false/use_object: true/' $FILE_PATH
+
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
+  --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
+  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_spatial \
+  --task_label oft_spatial_object_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
+  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
+  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
+  2>&1 | tee ckpts/oft_spatial_object_nulllang.txt
+
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
+  --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
+  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_10 \
+  --task_label oft_10_object_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
+  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
+  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
+  2>&1 | tee ckpts/oft_10_object_nulllang.txt
+
+# pos (swap)
+sed -i 's/use_object: true/use_object: false/' $FILE_PATH
+sed -i 's/use_swap: false/use_swap: true/' $FILE_PATH
+
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
+  --task_suite_name libero_spatial --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
+  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_spatial \
+  --task_label oft_spatial_swap_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
+  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
+  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
+  2>&1 | tee ckpts/oft_spatial_swap_nulllang.txt
+
+python $EVAL_SCRIPT \
+  --pretrained_checkpoint $CKPT_OFT --substep_completion_threshold $OFT_SUBSTEP_COMPLETION_THRESHOLD \
+  --task_suite_name libero_10 --e_decoding False --save_video False --use_substep_decomposition $OFT_USE_SUBSTEP_DECOMPOSITION \
+  --num_trials_per_task $NUM_TRIALS_PER_TASK --evaluation_config_path $FILE_PATH --unnorm_key libero_10 \
+  --task_label oft_10_swap_nulllang --use_eos_detection $OFT_USE_EOS_DETECTION --use_proprio $OFT_USE_PROPRIO \
+  --use_l1_regression $OFT_USE_L1_REGRESSION --use_bddl_language $OFT_USE_BDDL_LANGUAGE --auto_regression $OFT_AUTO_REGRESSION \
+  --num_images_in_input $OFT_NUM_IMAGES_IN_INPUT --null_instruction True \
+  2>&1 | tee ckpts/oft_10_swap_nulllang.txt
+
+sed -i 's/use_swap: true/use_swap: false/' $FILE_PATH
 
 echo "Language Dropping experiments done. Results in ckpts/."
