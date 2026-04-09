d90a1ed49d8663a50317e5ad96d9f5443c286863
Yuhang <yuhang@topsoftint.com>
2026-04-09 11:36:33 +0800

    apd_L1_tuned

--- auto_eval_nv40_pro.sh
+++ auto_eval_nv40_pro.sh
@@ -32,10 +32,10 @@ python $EVAL_SCRIPT \
 
 
 
-python $EVAL_SCRIPT \
-  --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold $SUBSTEP_COMPLETION_THRESHOLD\
-  --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition $USE_SUBSTEP_DECOMPOSITION\
-  --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label ${TASK_LABEL_PREFIX}_goal_lan --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --use_bddl_language $USE_BDDL_LANGUAGE --auto_regression $AUTO_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT
+# python $EVAL_SCRIPT \
+#   --pretrained_checkpoint $PRETRAINED_CHECKPOINT --substep_completion_threshold $SUBSTEP_COMPLETION_THRESHOLD\
+#   --task_suite_name libero_goal --e_decoding False --save_video False --use_substep_decomposition $USE_SUBSTEP_DECOMPOSITION\
+#   --num_trials_per_task 50 --evaluation_config_path experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml  --unnorm_key libero_goal --task_label ${TASK_LABEL_PREFIX}_goal_lan --use_eos_detection $USE_EOS_DETECTION --use_proprio $USE_PROPRIO --use_l1_regression $USE_L1_REGRESSION --use_bddl_language $USE_BDDL_LANGUAGE --auto_regression $AUTO_REGRESSION --num_images_in_input $NUM_IMAGES_IN_INPUT
 
 
 
