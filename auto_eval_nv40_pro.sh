3afc56d91767d39866bf1bc0b8c4d8980b07e197
Yuhang <yuhang@topsoftint.com>
2026-05-13 18:35:23 +0800

    saga on h100 eval

--- auto_eval_nv40_pro.sh
+++ auto_eval_nv40_pro.sh
@@ -3,7 +3,7 @@ echo "Running Evaluations Automatically ------------------------------"
 FARM_USER=sgyson10
 FILE_PATH="experiments/robot/libero/LIBERO-PRO/evaluation_config.yaml"
 PRETRAINED_CHECKPOINT="ckpt/ckpoints/saga_h100_24"
-TASK_LABEL_PREFIX="saga_rl_step24"
+TASK_LABEL_PREFIX="saga_rl_step24_h100"
 USE_EOS_DETECTION=False
 EVAL_SCRIPT="experiments/robot/libero/run_libero_pro_eval_substep.py"
 USE_PROPRIO=False
